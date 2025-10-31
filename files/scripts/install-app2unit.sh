e2be543f66c7bc48b1b6018d193b0d024451568a
Leopold Luley <git@leopoldluley.de>
2025-10-31 12:58:18 +0100

    Attempt manual installation of app2unit.

--- files/scripts/install-app2unit.sh
+++ files/scripts/install-app2unit.sh
@@ -4,9 +4,15 @@ set -euo pipefail
 
 REPO=Vladimir-csp/app2unit
 
-mkdir app2unit
-curl -s -H "Accept: application/vnd.github.v3+json" "https://api.github.com/repos/$REPO/tags" | jq '.[0].tarball_url' | xargs -n1 curl -sL | tar -xz -C app2unit --strip-components=1
-cd app2unit
-env PREFIX=/usr make install-bin
-cd ..
-rm -rf app2unit
+mkdir app2unit-latest
+curl -s -H "Accept: application/vnd.github.v3+json" "https://api.github.com/repos/$REPO/tags" | jq '.[0].tarball_url' | xargs -n1 curl -sL | tar -xz -C app2unit-latest --strip-components=1
+
+install -Dpm755 app2unit-latest/app2unit -t /usr/bin
+rm -rf app2unit-latest
+
+ln -sfT /usr/bin/app2unit /usr/bin/app2unit-open
+ln -sfT /usr/bin/app2unit /usr/bin/app2unit-open-scope
+ln -sfT /usr/bin/app2unit /usr/bin/app2unit-open-service
+ln -sfT /usr/bin/app2unit /usr/bin/app2unit-term
+ln -sfT /usr/bin/app2unit /usr/bin/app2unit-term-scope
+ln -sfT /usr/bin/app2unit /usr/bin/app2unit-term-service
