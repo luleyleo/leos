6c1c205bc70e9ce347fb5e478c08f5a33f345619
Leopold Luley <git@leopoldluley.de>
2025-10-31 11:14:10 +0100

    Install app2unit to /usr.

--- files/scripts/install-app2unit.sh
+++ files/scripts/install-app2unit.sh
@@ -7,6 +7,6 @@ REPO=Vladimir-csp/app2unit
 mkdir app2unit
 curl -s -H "Accept: application/vnd.github.v3+json" "https://api.github.com/repos/$REPO/tags" | jq '.[0].tarball_url' | xargs -n1 curl -sL | tar -xz -C app2unit --strip-components=1
 cd app2unit
-make install-bin
+env PREFIX=/usr make install-bin
 cd ..
 rm -rf app2unit
