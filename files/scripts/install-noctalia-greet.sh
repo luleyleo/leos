0b404509139394edfa8fcd1a618617027e613087
Leopold Luley <git@leopoldluley.de>
2025-11-10 08:59:14 +0100

    Small script clean-up.

--- files/scripts/install-noctalia-greet.sh
+++ files/scripts/install-noctalia-greet.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 REPO=noctalia-dev/noctalia-greet
 QS_CONFIG=/etc/xdg/quickshell
 
-mkdir -p /etc/xdg/quickshell
+mkdir -p "$QS_CONFIG"
 
 git clone --depth 1 "https://github.com/$REPO.git" "$QS_CONFIG/noctalia-greet"
 rm -rf "$QS_CONFIG/noctalia-greet/.git"
--- files/scripts/install-noctalia-shell.sh
+++ files/scripts/install-noctalia-shell.sh
@@ -5,6 +5,6 @@ set -euo pipefail
 REPO=noctalia-dev/noctalia-shell
 QS_CONFIG=/etc/xdg/quickshell
 
-mkdir -p /etc/xdg/quickshell
+mkdir -p "$QS_CONFIG"
 
 curl -s -H "Accept: application/vnd.github.v3+json" "https://api.github.com/repos/$REPO/releases/latest" | jq '.assets[0].browser_download_url' | xargs -n1 curl -sL | tar -xz -C "$QS_CONFIG" --transform=s/noctalia-release/noctalia-shell/
