408225c12d0acdbe29639c3c6188dac78564ebe5
Leopold Luley <git@leopoldluley.de>
2025-12-07 13:54:02 +0100

    Leave Matugen installation to dms package.

--- files/scripts/install-matugen.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-REPO=InioX/matugen
-TARGET=/usr/bin/
-
-curl -s -H "Accept: application/vnd.github.v3+json" "https://api.github.com/repos/$REPO/releases/latest" | jq '.assets[0].browser_download_url' | xargs -n1 curl -sL | tar -xz -C "$TARGET" matugen
